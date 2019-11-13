49781b893e760cc868d6d7f22a3a033c9f61143f
aaron.kai.hubbard <aaron.kai.hubbard@gmail.com>
2019-11-13 10:32:35 -0500

    number gessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number ||= rand(100)
+SECRET_NUMBER ||= rand(100)
 
 get '/' do
-  erb :index, :locals => {:secret_number => secret_number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message} 
+end
+
+
+def check_guess(guess)
+  if guess.to_i > SECRET_NUMBER
+    message = "Too High!"
+  elsif guess.to_i < SECRET_NUMBER
+    message = "Too Low!"
+  else
+    message = "You Got It Right!"
+  end
+  return message
 end
