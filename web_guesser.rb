f264e6be3692f340a8d6e04ba508c388eca9e94c
aaron.kai.hubbard <aaron.kai.hubbard@gmail.com>
2019-11-13 11:15:43 -0500

    added color to background that relates to guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,11 +2,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER ||= rand(100)
+background_color = 'white'
 
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message} 
+  erb :index, :locals => {:secret_number => SECRET_NUMBER, 
+                          :guess => guess,
+                          :message => message,
+                          :background_color => background_color(guess)} 
 end
 
 
@@ -19,4 +23,17 @@ def check_guess(guess)
     message = "You Got It Right!"
   end
   return message
+end
+
+def background_color(guess)
+  difference = (guess.to_i - SECRET_NUMBER).abs
+  if difference > 10
+    return "firebrick"
+  elsif difference <= 10 && difference > 3
+    return "pink"
+  elsif difference <= 3 && difference > 0
+    return 'paleGreen'
+  elsif difference == 0
+    return 'royalBlue'
+  end
 end
